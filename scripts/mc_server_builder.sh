85eb0eda89f42ee8567080f557c894c9ec77124b
Nicolás Castellán <cnicolas.developer@gmail.com>
2021-07-08 18:46:32 -0300

    ✨ Update mc server to 1.17.1

--- scripts/mc_server_builder.sh
+++ scripts/mc_server_builder.sh
@@ -126,8 +126,8 @@ fi
 
 # TODO: Keep link up to date.
 # Copy the download link from https://www.minecraft.net/en-us/download/server if there's a newer version.
-version="1.16.5"
-download_link="https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar"
+version="1.17.1"
+download_link="https://launcher.mojang.com/v1/objects/a16d67e5807f57fc4e550299cf20226194497dc2/server.jar"
 
 Animate() {
 	CICLE=('|' '/' '-' '\')
