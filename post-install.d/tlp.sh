187c21928c4df24155459d71ed18592d3946e03c
Nicolás Castellán <cnicolas.developer@gmail.com>
2021-08-03 21:21:27 -0300

    🔨 Improved config choices for HandleLidSwitch

--- post-install.d/tlp.sh
+++ post-install.d/tlp.sh
@@ -11,5 +11,19 @@ sudo systemctl restart tlp
 
 printf "%s\e[00m\n" $([ $? -eq 0 ] && printf "\e[32mSuccess" || printf "\e[31mFail")
 
-read -rp "Do you want to suspend the OS when you close the lid? (laptops only) (Y/n) "
-[[ ${REPLY,,} == "y" ]] && sudo sed -i 's/#HandleLidSwitch=suspend/HandleLidSwitch=suspend/' /etc/systemd/logind.conf
+printf "If you have a laptop. What do you want to do when you close the lid?\n"
+select s in ignore poweroff suspend hibernate "I don't have a laptop"; do
+case $s in
+	ignore)                  CHOICE=ignore    ;;
+	poweroff)                CHOICE=poweroff  ;;
+	suspend)                 CHOICE=suspend   ;;
+	hibernate)               CHOICE=hibernate ;;
+	"I don't have a laptop") CHOICE=none      ;;
+	*) echo "Option $s not recognized."; continue ;;
+esac; break; done
+
+if [ "$CHOICE" != "none" ]; then
+	sudo sed -i "s/#HandleLidSwitch=suspend/HandleLidSwitch=$CHOICE/" /etc/systemd/logind.conf
+	sudo systemctl restart systemd-logind.service
+fi
+unset CHOICE
