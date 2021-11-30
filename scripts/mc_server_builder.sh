d1eaa0e4ba3fce5976f7dc49240b7ca25fef6cf4
Nicolás Castellán <cnicolas.developer@gmail.com>
2021-11-30 14:43:21 -0300

    ⚡ Update minecraft server link to 1.18

--- scripts/mc_server_builder.sh
+++ scripts/mc_server_builder.sh
@@ -7,8 +7,8 @@
 
 # TODO: Keep link up to date.
 # Copy the download link from https://www.minecraft.net/en-us/download/server if there's a newer version.
-version="1.17.1"
-download_link="https://launcher.mojang.com/v1/objects/a16d67e5807f57fc4e550299cf20226194497dc2/server.jar"
+version="1.18"
+download_link="https://launcher.mojang.com/v1/objects/3cf24a8694aca6267883b17d934efacc5e44440d/server.jar"
 
 # Exit codes:
 # 1   - Java not found
